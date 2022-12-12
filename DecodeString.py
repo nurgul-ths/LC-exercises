9a14adab7cbae25923fb28d18d547e55486b799a
unknown <nurgul.akhshatayeva@ucalgary.ca>
2022-12-12 09:41:34 -0700

    lc 394

---
+++ DecodeString.py
@@ -0,0 +1,48 @@
+'''
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+'''
+
+class Solution:
+
+    def decodeString(self, s:str) -> str:
+        stack = []
+        s=''
+        for c in s:
+            if c != ']':
+                stack.append(c)
+            else:
+                k = ''
+                while stack[-1]!='[':
+                    k = stack.pop() + k
+                
+                stack.pop()
+
+                while stack and stack[-1].isdigit():
+                    num = ''
+                    num = stack.pop()+num 
+                
+            stack.append(int(num)*k)
+        return "".join(stack)
+
+
